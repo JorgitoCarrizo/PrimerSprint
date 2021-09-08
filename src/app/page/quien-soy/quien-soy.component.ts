c07a66eb91d3314c319cfbdb8a20ba484840f543
Jorge Carrizo <jorgitocarrizo@gmail.com>
2021-09-08 01:31:48 -0300

    first commit

--- src/app/page/quien-soy/quien-soy.component.ts
+++ src/app/page/quien-soy/quien-soy.component.ts
@@ -7,9 +7,12 @@ import { Router } from '@angular/router';
 })
 export class QuienSoyComponent implements OnInit {
 
-  constructor() { }
+  constructor(private rutas:Router) { }
 
   ngOnInit(): void {
   }
 
+  volver(){
+    this.rutas.navigate(['bienvenido'])
+  }
 }
